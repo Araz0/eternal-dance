f1e4f1e6ad3b6608d7f16d7d9c5b1794a965e91c
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2025-02-25 18:31:32 +0100

    feat: simple example of streaming out and in

--- backend/streamer/record.js
+++ backend/streamer/record.js
@@ -1,15 +1,14 @@
 const { exec } = require('child_process')
 
-const rtspUrl = 'rtsp://127.0.0.1:554/tdvid'
-// The -movflags flag helps FFmpeg write the MP4 file in a way that makes it playable
-const command = `ffmpeg -rtsp_transport tcp -i "${rtspUrl}" -t 5 -c copy output.mp4`
+const rtspUrl = 'rtsp://127.0.0.1:554/tdvidstream'
+const timestampNumber = Date.now()
+const command = `ffmpeg -rtsp_transport tcp -i "${rtspUrl}" -t 5 -c copy ${timestampNumber}.mp4`
 
 console.log('Recording started...')
-
 exec(command, (error, stdout, stderr) => {
   if (error) {
     console.error(`Recording error: ${error.message}`)
     return
   }
-  console.log('Recording complete! Output saved as output.mp4')
+  console.log(`Recording complete! Output saved as ${timestampNumber}.mp4`)
 })
