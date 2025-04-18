cbc2b5d355d6ac9aa1b574291f05bf11dc0c800f
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2025-04-18 17:43:11 +0200

    feat: adapt the backend to new api specs

--- backend/streamer/server.js
+++ backend/streamer/server.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import { recordStream } from './record.js'
-import { uploadVideo } from './upload.js'
+import { uploadVideo, uploadImage } from './upload.js'
 // import { UDPServer } from './touchdesigner-server.js'
 import { Highlighter } from './Highlighter.js'
 
@@ -12,28 +12,40 @@ if (!fs.existsSync('./streams')) {
   fs.mkdirSync('./streams', { recursive: true })
 }
 
-console.log('Triggering video recording...')
+console.time('recordStream')
+// timer running on the side asynchronously
+const recordingTimer = setInterval(() => {
+  const elapsedTime = Math.floor((Date.now() - timestamp) / 1000)
+  console.log(`${elapsedTime} seconds elapsed`)
+}, 1000)
+
 const videoPath = await recordStream({
   rtspUrl: process.env.RTSP_URL || 'rtsp://127.0.0.1:554/tdvidstream',
   duration: recordingDurationInSeconds,
   outputFile: './streams/' + timestamp + '.mp4',
 })
+console.timeEnd('recordStream')
 console.log('Video saved to:', videoPath)
 
 const highlighter = new Highlighter(videoPath)
 try {
   const { finalVideo, thumbnail, croppedVideo, croppedThumbnail } =
     await highlighter.highlight()
-  console.log('Final paths:', {
+  console.log('local paths:', {
     finalVideo,
     thumbnail,
     croppedVideo,
     croppedThumbnail,
   })
 
-  console.log('Uploading video...')
-  const onlineLink = await uploadVideo(croppedVideo)
-  console.log('Video uploaded:', onlineLink)
+  console.log('Uploading...')
+  const onlineReelLink = await uploadVideo(croppedVideo)
+
+  console.log('## ~ onlineReelLink:', onlineReelLink)
+
+  const onlineThumbnailLink = await uploadImage(croppedThumbnail)
+
+  console.log('## ~ onlineThumbnailLink:', onlineThumbnailLink)
 } catch (error) {
   console.error('Error:', error)
 }
@@ -42,53 +54,6 @@ try {
 // const udpServer = new UDPServer('127.0.0.1', 41234) // Replace with your desired IP and port
 
 // udpServer.onCallback(async (currentValue) => {
-//   console.log('Received value from UDP:', currentValue)
 
-//   if (currentValue === 1 || currentValue === '1' || currentValue === 1.0) {
-
-//   } else {
-//     console.log('No action triggered for value:', currentValue)
-//   }
 // })
 // udpServer.start()
-
-// const VIDEO_PATH = '.\\streams\\3min.mp4'
-// const highlighter = new Highlighter(VIDEO_PATH)
-// try {
-//   const { finalVideo, thumbnail } = await highlighter.highlight()
-//   console.log('Final video path:', finalVideo)
-//   console.log('Thumbnail path:', thumbnail)
-// } catch (error) {
-//   console.error('Error:', error)
-// }
-
-// async function runRecordStream() {
-//   // Example usage (custom parameters):
-//   const videoPath = await recordStream({
-//     rtspUrl: process.env.RTSP_URL || 'rtsp://127.0.0.1:554/tdvidstream',
-//     duration: recordingDurationInSeconds,
-//     outputFile: './streams/' + timestamp + '.mp4',
-//   })
-//   console.log('Video saved to:', videoPath)
-
-//   // upload video online
-//   // try {
-//   //   console.log('will try to upload video now:', videoPath)
-//   //   // Upload the recorded video.
-//   //   const onlineLink = await uploadVideo(videoPath)
-//   //   console.log('Video uploaded:', onlineLink)
-//   // } catch (error) {
-//   //   console.error('Error uploading video file:', error)
-//   // }
-
-//   return videoPath
-// }
-
-// console.log(
-//   `Recording video stream with duration of ${recordingDurationInSeconds} seconds...`
-// )
-
-// console.time('runRecordStream')
-// runRecordStream().then(() => {
-//   console.timeEnd('runRecordStream')
-// })
--- backend/streamer/upload.js
+++ backend/streamer/upload.js
@@ -2,10 +2,19 @@ import fs from 'fs'
 import axios from 'axios'
 import FormData from 'form-data'
 
-const BASE_URL = 'https://eternal-dance.art/exps'
+const BASE_URL = 'https://api.eternal-dance.art'
+const API_ENDPOINT = BASE_URL + '/api.php'
+const UPLOAD_DIR = 'exps'
 
-// Example usage:
-// uploadVideo('./1740507935180.mp4')
+/**
+ * Uploads a video file to a remote server.
+ * @param {string} videoPath - The path to the video file to upload.
+ * @return {Promise<string>} - The URL of the uploaded video.
+ * @throws Will throw an error if the upload fails.
+ * @example
+ * uploadVideo('./path/to/video.mp4')
+ *   .then(url => console.log('Uploaded video URL:', url))
+ */
 export async function uploadVideo(videoPath) {
   // Create a new form and append the video file stream.
   const form = new FormData()
@@ -13,7 +22,7 @@ export async function uploadVideo(videoPath) {
 
   try {
     // Make the POST request with the form data.
-    const response = await axios.post(BASE_URL + '/api.php', form, {
+    const response = await axios.post(API_ENDPOINT, form, {
       headers: {
         ...form.getHeaders(),
       },
@@ -21,11 +30,45 @@ export async function uploadVideo(videoPath) {
       maxBodyLength: Infinity,
     })
     console.log('Upload successful:', response.data)
-    return BASE_URL + `/uploads/${response.data.file}`
+    return `${BASE_URL}/${UPLOAD_DIR}/${response.data.file}`
   } catch (error) {
     // Log detailed error information.
     console.error(
       'Error uploading video:',
+      error.response ? error.response.data : error.message
+    )
+  }
+}
+
+/**
+ * Uploads an image file to a remote server.
+ * @param {string} imagePath - The path to the image file to upload.
+ * @return {Promise<string>} - The URL of the uploaded image.
+ * @throws Will throw an error if the upload fails.
+ * @example
+ * uploadImage('./path/to/image.png')
+ *  .then(url => console.log('Uploaded image URL:', url))
+ */
+export async function uploadImage(imagePath) {
+  // Create a new form and append the image file stream.
+  const form = new FormData()
+  form.append('image', fs.createReadStream(imagePath))
+
+  try {
+    // Make the POST request with the form data.
+    const response = await axios.post(API_ENDPOINT, form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
+    })
+    console.log('Upload successful:', response.data)
+    return `${BASE_URL}/${UPLOAD_DIR}/${response.data.file}`
+  } catch (error) {
+    // Log detailed error information.
+    console.error(
+      'Error uploading image:',
       error.response ? error.response.data : error.message
     )
   }
