be900c630be58eb451e4f92549d5d4689ab54a7e
tanja <tanjanicole.g@gmx.at>
2025-04-27 10:43:06 +0200

    feat: new landing page layout

--- frontend/src/components/PaintingCanvas/PaintingCanvas.tsx
+++ frontend/src/components/PaintingCanvas/PaintingCanvas.tsx
@@ -18,7 +18,7 @@ const PaintingCanvasRaw = () => {
       let hue = 0;
 
       p.setup = () => {
-        const canvas = p.createCanvas(p.windowWidth, p.windowHeight*0.9);
+        const canvas = p.createCanvas(p.windowWidth, p.windowHeight * 1.0);
         canvas.parent(containerRef.current!);
         p.colorMode(p.HSL, 360, 100, 100, 100);
         p.background(0, 0, 0);
@@ -46,7 +46,7 @@ const PaintingCanvasRaw = () => {
       };
 
       p.windowResized = () => {
-        p.resizeCanvas(p.windowWidth, p.windowHeight*0.9);
+        p.resizeCanvas(p.windowWidth, p.windowHeight * 1);
       };
     }, containerRef.current);
 
@@ -62,8 +62,11 @@ const PaintingCanvasRaw = () => {
 
   return (
     <>
-      <div ref={containerRef} className={styles.canvasContainer} />
-      <div className={styles.overlay} />
+      <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+        <div ref={containerRef} className={styles.canvasContainer} />
+        <div className={styles.overlay} />
+        <p className={styles.logline}>An audiovisual installation where movement and emotion intertwine in an eternal dance of transformation.</p>
+      </div>
     </>
   );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import { PageContainer } from '../components'
-import Paragraph from '../components/Word'
+import Header from '../components/Header/Header'
+import MouseFollowImageGallery from '../components/MouseFollowImageGallery/MouseFollowImageGallery'
+import { PaintingCanvas } from '../components/PaintingCanvas'
+import About from '../components/About/About'
+import styles from './styles.module.css'
 
 export const Home = () => {
-  return (
-    <PageContainer>
-      <div style={{ height: '100vh' }}></div>
-      <Paragraph paragraph='An audiovisual installation where movement and emotion intertwine in an eternal dance of transformation.' />
-      <div style={{ height: '100vh' }}></div>
-    </PageContainer>
+  return (  
+    <>
+      <Header />
+      <PaintingCanvas />
+      {/* <p className={styles.logline}>An audiovisual installation where movement and emotion intertwine in an eternal dance of transformation.</p> */}
+      <About />
+      <MouseFollowImageGallery 
+        maxMovementRadius={100} 
+        movementIntensity={0.4} 
+        lerpFactor={0.15}
+      />
+    </>
   )
 }
